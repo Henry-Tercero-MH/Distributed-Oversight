a1b4b6b931fa496caff894b7d5071e339c45bcfc
Henry-Tercero-MH <tercerohernandezhenrymisael@gmail.com>
2024-08-17 15:39:21 -0600

    se agrego el fronted del modulo RFID, para movil y tablet

--- DO/src/Paginas/RFID/RFID.jsx
+++ DO/src/Paginas/RFID/RFID.jsx
@@ -13,62 +13,56 @@ const RFID = () => {
         <form action="" className={styles.formulario}>
           <div className={styles.contenedor1}>
             <div className={styles.cajaSemaforo}>
-              <div className={styles.semaforoR}>
-                <h1>df</h1>
-              </div>
-              <div className={styles.semaforoA}>
-                <h1>df</h1>
-              </div>
-              <div className={styles.semaforoV}>
-                <h1>df</h1>
-              </div>
+              <div className={styles.semaforoR}></div>
+              <div className={styles.semaforoA}></div>
+              <div className={styles.semaforoV}></div>
             </div>
             <div className={styles.tituloEstado}>
-              <Label>Solvente</Label>
-            </div>
-            <div className={styles.imagenVehiculo}>
-              <img src={imagenVehiculo} alt="" />
+              <Label>Reporte de Robo</Label>
             </div>
           </div>
-          <div className={styles.contenedor2}>
-            <div className={styles.datosTitulo}>
-              <Label>Datos del Vehiculo</Label>
+          <div className={styles.cajaMadre}>
+            <div className={styles.contenedor2}>
+              <div className={styles.imagenVehiculo}>
+                <img src={imagenVehiculo} alt="" />
+              </div>
+              <div className={styles.datosTitulo}>
+                <Label>Datos del Vehiculo</Label>
+              </div>
+              <MostrarTexto dato={"JONONSINSKS"}>USO</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>Tipo</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>Linea</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>Chasis</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>Serie</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>Asientos</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>Color</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>Placa</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>Modelo</MostrarTexto>
             </div>
-            <MostrarTexto dato={"JONONSINSKS"}>USO</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>Tipo</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>Linea</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>Chasis</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>Serie</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>Asientos</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>Color</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>Placa</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>Modelo</MostrarTexto>
-          </div>
-          <div className={styles.contenedor3}>
-            <div className={styles.imagenConductor}>
-              <img src={imagenUsuario} alt="" />
-            </div>
-          </div>
-          <div className={styles.contenedor2}>
-            <div className={styles.datosTitulo}>
-              <Label>Datos del Conductor</Label>
-            </div>
-            <MostrarTexto dato={"JONONSINSKS"}>nombre</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>cui</MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>nit</MostrarTexto>
+            <div className={styles.contenedor2}>
+              <div className={styles.imagenConductor}>
+                <img src={imagenUsuario} alt="" />
+              </div>
+              <div className={styles.datosTitulo}>
+                <Label>Datos del Conductor</Label>
+              </div>
+              <MostrarTexto dato={"JONONSINSKS"}>nombre</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>cui</MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>nit</MostrarTexto>
 
-            <div className={styles.datosTitulo}>
-              <Label>Datos del Reporte</Label>
+              <div className={styles.datosTitulo}>
+                <Label>Datos del Reporte</Label>
+              </div>
+              <MostrarTexto dato={"JONONSINSKS"}>
+                <img src={iconUbicacion} alt="" width="40px" height="40px" />
+              </MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>
+                <img src={iconCalendar} alt="" width="40px" height="40px" />
+              </MostrarTexto>
+              <MostrarTexto dato={"JONONSINSKS"}>
+                <img src={iconReloj} alt="" width="40px" height="40px" />
+              </MostrarTexto>
             </div>
-            <MostrarTexto dato={"JONONSINSKS"}>
-              <img src={iconUbicacion} alt="" width="40px" height="40px" />
-            </MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>
-              <img src={iconCalendar} alt="" width="40px" height="40px" />
-            </MostrarTexto>
-            <MostrarTexto dato={"JONONSINSKS"}>
-              <img src={iconReloj} alt="" width="40px" height="40px" />
-            </MostrarTexto>
           </div>
         </form>
       </section>
--- DO/src/componentes/LabelTexto/MostrarTexto.jsx
+++ DO/src/componentes/LabelTexto/MostrarTexto.jsx
@@ -3,7 +3,7 @@ import styles from "./MostrarTexto.module.css";
 const MostrarTexto = ({ children, dato }) => {
   return (
     <div className={styles.cajaTexto}>
-      <label htmlFor="mostrar">{children}:</label>
+      <label htmlFor="mostrar">{children}</label>
       <span>{dato}</span>
     </div>
   );
